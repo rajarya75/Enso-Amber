da245ffcf0e9b3ca3a7cf29a22722b014aa1dbbf
rajarya75 <rajarya75@gmail.com>
2024-08-27 08:20:29 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".btn-back").click(function () {
+    $(".banner").toggleClass("full-banner");
+  });
+
+  $(".humbergure").click(function () {
+    $(".banner").toggleClass("full-banner");
+  });
+});
